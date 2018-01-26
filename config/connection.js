5cbcbde69e0d866f0a33f0e6ea152d4abf9119c7
Philipp Wickart <phwickart@gmail.com>
2018-01-25 21:16:52 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,17 @@ connection.connect(function(err) {
     return;
   }
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "",
+		database: "burger_db"
+	});
+};
+
   console.log("connected as id " + connection.threadId);
 });
 
